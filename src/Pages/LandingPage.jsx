80370b7dc796928ef5e955a66b9afd51a0250ee6
chikamso-cmd <nelsonjude2@gmail.com>
2026-02-27 13:17:05 +0100

    added the animation to the landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,27 +5,6 @@ import Footer from "../components/Footer";
 import Onboarding from "../components/Onboarding";
 import { motion } from "framer-motion";
 
-const containerVariants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.15,
-    },
-  },
-};
-
-const itemVariants = {
-  hidden: { opacity: 0, y: 20 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.5,
-      ease: "easeOut",
-    },
-  },
-};
 
 export default function LandingPage() {
   return (
